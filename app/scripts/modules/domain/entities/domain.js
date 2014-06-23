a01533ae001adc31cf9c7830542acc9e9ea3c3c4
George Connor <grgcnnr@gmail.com>
2014-06-23 19:18:50 +1200

    colors for domains - need to be refined !!!

--- app/scripts/modules/domain/entities/domain.js
+++ app/scripts/modules/domain/entities/domain.js
@@ -40,20 +40,46 @@ define(['app'], function(App) {
             var fakeDomains = new Entities.DomainCollection([
             {
                 id: 0,
-                name: 'Employment',
-                slug: 'employment'
+                name: 'Personal meaning',
+                slug: 'personal-meaning',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'personal-meaning',
             }, {
                 id: 1,
-                name: 'Personal meaning',
-                slug: 'meaning',
+                name: 'Self-belief',
+                slug: 'self-belief',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'self-belief',
             }, {
                 id: 2,
-                name: 'Lifestyle',
-                slug: 'lifestyle',
+                name: 'Relationships',
+                slug: 'relationships',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'relationships',
             }, {
                 id: 3,
+                name: 'Self-management',
+                slug: 'self-management',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'self-management',
+            }, {
+                id: 4,
+                name: 'Work',
+                slug: 'work',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'work',
+            }, {
+                id: 5,
+                name: 'Lifestyle',
+                slug: 'lifestyle',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'lifestyle',
+            }, {
+                id: 6,
                 name: 'Services',
                 slug: 'services',
+                desc: 'A very short description of the challenge. About this many words',
+                cssClass: 'services',
             }
             ]);
 
