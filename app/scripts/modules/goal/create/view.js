662f7e983b9f1fbe5806e4ed850dc7a8dc044bb7
George Connor <grgcnnr@gmail.com>
2014-06-30 14:22:55 +1200

    refactored goal adder js and some styles.

--- app/scripts/modules/goal/create/view.js
+++ app/scripts/modules/goal/create/view.js
@@ -6,20 +6,40 @@ define(['app', 'templates', 'dust'], function(App) {
             template: 'goal_create',
 
             events: {
-                'click #add_goal': 'added',
-                'click .auto_goal': 'addGoal'
+                // 'click #add_goal': 'added',
+                'click .auto_goal': 'addGoal',
+                'keyup #goal_name': 'addGoalform'
+            },
+
+            appendGoal: function(content){
+                var newgoal = $('<div class="activity__personal-goals__goal">' + content + '</div>');
+                newgoal.hide();
+                this.$el.find('#goals').append(newgoal);
+                newgoal.slideDown();
+            },
+
+            addGoalform: function(e){
+                e.preventDefault();
+                if(e.keyCode === 13 ){
+                    this.appendGoal( this.$el.find('#goal_name').val() );
+                    this.$el.find('input').val('');
+                }
             },
 
             addGoal: function(click) {
-                this.$el.find('#goals').append('<div>' + click.target.innerText + '</div>');
-                click.target.remove();
+                this.appendGoal( click.target.innerText );
+                $(click.target).slideUp();
+                //.slideUp();
             },
 
-            added: function(e) {
+/*            added: function(e) {
                 console.log(e);
-                this.$el.find('#goals').append('<div>' + this.$el.find('#goal_name').val() + '</div>');
+                this.$el.find('#goals')
+                    .append('<div class="activity__personal-goals__goal">' + this.$el.find('#goal_name').val() + '</div>')
+                    .hide()
+                    .slideDown();
                 this.$el.find('form')[0].reset();
-            },
+            },*/
 
             serializeData: function() {
                 var data = {};
