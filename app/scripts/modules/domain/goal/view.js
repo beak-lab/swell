9b2a0a8b6039e088580ad5094e09e70500ef0144
George Connor <grgcnnr@gmail.com>
2014-07-04 15:04:57 +1200

    work on #64

--- app/scripts/modules/domain/goal/view.js
+++ app/scripts/modules/domain/goal/view.js
@@ -1,5 +1,5 @@
 'use strict';
-define(['app', 'templates', 'dust', 'dustIterate'], function(App) {
+define(['app', 'templates', 'dust', 'dustIterate', 'jquery.velocity'], function(App) {
     App.module('Domain.Show.View', function(View, App, Backbone, Marionette) {
         View.Layout = Marionette.Layout.extend({
             template: 'domain_goal_show',
@@ -13,6 +13,41 @@ define(['app', 'templates', 'dust', 'dustIterate'], function(App) {
         View.Goals = Marionette.ItemView.extend({
             template: 'domain_goals',
 
+            events: {
+                'keyup #goal_name': 'addGoalform',
+                'click #personal-goal-add-form' : 'addGoalform',
+            },
+
+
+            appendGoal: function(text) {
+
+                var newgoalDiv = $('<label class="mystuff__goals__goal"><input class="mystuff__goals__goal-checkbox checkboxable__checkbox" type="checkbox" /><div class="activity-goal__text">' + text + '</div></label>');
+                //newgoalDiv.hide();
+                newgoalDiv.css({'height' :  0, 'min-height' : 0});
+
+                // this.$el.find('.activity__personal-goals').addClass('has-goals').prepend(newgoalDiv);
+                this.$el.find('#goals').append(newgoalDiv);
+                newgoalDiv.velocity({
+                    'min-height' : '5rem'
+                }, 500).promise().done(function(){
+                    newgoalDiv.css({'height' :  '', 'min-height' : ''});
+                });
+                //newgoalDiv.slideDown();
+            },
+
+            addGoalform: function(e) {
+
+                e.preventDefault();
+                if (e.keyCode === 13 || $(e.target).hasClass('activity-goal__icon')) {
+                    var input = this.$el.find('#goal_name');
+                    console.log(input);
+                    if( input.val().trim() ) {
+                        this.appendGoal(input.val());
+                        input.val('');
+                    }
+                }
+            },
+
             serializeData: function() {
                 return {
                     goals: this.options.goals
@@ -23,3 +58,5 @@ define(['app', 'templates', 'dust', 'dustIterate'], function(App) {
 
     return App.Domain.Show.View;
 });
+
+
